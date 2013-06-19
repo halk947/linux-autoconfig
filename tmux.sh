cd0b976de308648e5ed3ef80cb940b8317879834
Tanky Woo <wtq1990@gmail.com>
2013-06-19 16:32:08 +0800

    script tmux

---
+++ tmux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Tanky Woo@2013-06-19 10:51:15
+# About: 
+
+tmux start-server
+
+if ! $(tmux has-session -t 'tankywoo'); then
+	tmux new-session -d -s 'tankywoo' -n 'tankywoo' # -d *
+	tmux select-window -t 'tankywoo'
+	tmux split-window -h -p 60
+	tmux select-pane -t 1
+	tmux split-window -v -p 25
+	tmux send-keys -t 0 'ipython' C-m
+	# The C-m at the end is interpreted by Tmux as the enter key.
+
+	tmux new-window -n 'ops-dev'
+
+	tmux select-window -t 'tankywoo'
+	tmux select-pane -t 1
+fi
+
+tmux attach-session -d -t 'tankywoo'
